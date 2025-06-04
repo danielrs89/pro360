b71e829b760b0510afb50d62d8598571b9f750cf
danie <danie@danie.com>
2025-06-04 08:53:28 +0200

    init

---
+++ src/app/models/Providers.ts
@@ -0,0 +1,8 @@
+export default interface Providers {
+    id_provider: number
+    category_provider: string,
+    name_provider: string,
+    cif_provider: string,
+    email_provider: string,
+    phone_provider: string
+}
